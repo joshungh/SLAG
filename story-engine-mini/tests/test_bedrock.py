1659067db1b6029f3456e503d5fcf51a3f700d61
mango31 <31261308+mango31@users.noreply.github.com>
2025-01-08 20:32:36 -0700

    Revert "Story engine vmini"

--- story-engine-mini/tests/test_bedrock.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from src.core.clients.bedrock import get_bedrock_client
-
-def test_bedrock_client_creation():
-    """Test that Bedrock client can be created"""
-    client = get_bedrock_client()
-    assert client is not None
-    # Verify client has expected methods
-    assert hasattr(client, 'invoke_model') 
--- vMini-engine/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Create necessary directories if they don't exist
-mkdir -p output logs
-
-# Build and run the Docker containers
-docker-compose up --build 
